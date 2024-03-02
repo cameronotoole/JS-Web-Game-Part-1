ec051ce33fa71934b77f6d02f0bfa1819b214869
cameron otoole <cameronotoole72@gmail.com>
2024-03-02 10:05:08 -0500

    changes to const

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function tile(url, left, bottom, width, height) {
-    for (let h = 0; h < height; h++) {
-        for (let w = 0; w < width; w++) {
+    for (const h = 0; h < height; h++) {
+        for (const w = 0; w < width; w++) {
             newImage(url, left + w * 100, bottom + h * 100)
         }
     }
 }
 
 function newImage(url, left, bottom) {
-    let object = document.createElement('img')
+    const object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
     object.style.left = left + 'px'
@@ -25,9 +25,9 @@ function newItem(url, left, bottom) {
 }
 
 
-let horizon = window.innerHeight / 1.65
-let heightOfSky = window.innerHeight - horizon
-let heightOfGrass = horizon
+const horizon = window.innerHeight / 1.65
+const heightOfSky = window.innerHeight - horizon
+const heightOfGrass = horizon
 
 tile('assets/sky.png', 0, horizon, window.innerWidth / 100, heightOfSky / 100)
 tile('assets/grass.png', 0, 0, window.innerWidth / 100, heightOfGrass / 100)
